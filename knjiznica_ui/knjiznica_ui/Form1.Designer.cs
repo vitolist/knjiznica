fb537f268ac534fa16d9b2c7a32ae0008e6734e4
vitolist <61635438+vitolist@users.noreply.github.com>
2023-10-25 11:47:19 +0200

    Add files via upload

---
+++ knjiznica_ui/knjiznica_ui/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace knjiznica_ui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.imeTextBox = new System.Windows.Forms.TextBox();
+            this.prezimeTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.dodajButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.autorTextBox = new System.Windows.Forms.TextBox();
+            this.nazivTextBox = new System.Windows.Forms.TextBox();
+            this.korisniciList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(251, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KNJIGE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "KORISNICI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(655, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "POSUDBE";
+            // 
+            // imeTextBox
+            // 
+            this.imeTextBox.Location = new System.Drawing.Point(82, 90);
+            this.imeTextBox.Name = "imeTextBox";
+            this.imeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.imeTextBox.TabIndex = 3;
+            // 
+            // prezimeTextBox
+            // 
+            this.prezimeTextBox.Location = new System.Drawing.Point(82, 117);
+            this.prezimeTextBox.Name = "prezimeTextBox";
+            this.prezimeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.prezimeTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ime:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Prezime:";
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(17, 150);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(165, 23);
+            this.registerButton.TabIndex = 7;
+            this.registerButton.Text = "Registriraj";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            // 
+            // dodajButton
+            // 
+            this.dodajButton.Location = new System.Drawing.Point(256, 150);
+            this.dodajButton.Name = "dodajButton";
+            this.dodajButton.Size = new System.Drawing.Size(165, 23);
+            this.dodajButton.TabIndex = 12;
+            this.dodajButton.Text = "Dodaj";
+            this.dodajButton.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(253, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Autor:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(253, 96);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Naziv:";
+            // 
+            // autorTextBox
+            // 
+            this.autorTextBox.Location = new System.Drawing.Point(321, 117);
+            this.autorTextBox.Name = "autorTextBox";
+            this.autorTextBox.Size = new System.Drawing.Size(100, 20);
+            this.autorTextBox.TabIndex = 9;
+            // 
+            // nazivTextBox
+            // 
+            this.nazivTextBox.Location = new System.Drawing.Point(321, 90);
+            this.nazivTextBox.Name = "nazivTextBox";
+            this.nazivTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nazivTextBox.TabIndex = 8;
+            // 
+            // korisniciList
+            // 
+            this.korisniciList.FormattingEnabled = true;
+            this.korisniciList.Location = new System.Drawing.Point(19, 179);
+            this.korisniciList.Name = "korisniciList";
+            this.korisniciList.Size = new System.Drawing.Size(120, 199);
+            this.korisniciList.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.korisniciList);
+            this.Controls.Add(this.dodajButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.autorTextBox);
+            this.Controls.Add(this.nazivTextBox);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.prezimeTextBox);
+            this.Controls.Add(this.imeTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox imeTextBox;
+        private System.Windows.Forms.TextBox prezimeTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Button dodajButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox autorTextBox;
+        private System.Windows.Forms.TextBox nazivTextBox;
+        private System.Windows.Forms.ListBox korisniciList;
+    }
+}
+
---
+++ knjiznica_ui/knjiznica_ui/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace knjiznica_ui
+{
+
+    public partial class Form1 : Form
+    {
+        Knjiznica knjiznica = new Knjiznica();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            knjiznica.dodajKnjigu("Hamlet");
+            knjiznica.dodajKnjigu("Hamlet");
+            knjiznica.dodajKnjigu("Lovac");
+            knjiznica.dodajKnjigu("Vuk");
+            knjiznica.dodajKnjigu("Crvenkapica");
+            knjiznica.dodajKnjigu("Crvenkapica");
+            knjiznica.dodajKnjigu("Crvenkapica");
+            knjiznica.dodajKnjigu("Crvenkapica");
+            knjiznica.dodajKnjigu("Snjeguljica");
+            knjiznica.dodajKnjigu("Hamlet");
+
+            knjiznica.ispisKnjiga();
+            Console.WriteLine();
+
+            knjiznica.dodajKorisnika("Ivan");
+            knjiznica.dodajKorisnika("Marko");
+            knjiznica.dodajKorisnika("Ivan");
+            knjiznica.dodajKorisnika("Vito");
+            knjiznica.dodajKorisnika("Mirko");
+            knjiznica.dodajKorisnika("Bruno");
+
+            knjiznica.posudiKnjigu("2", "3");
+            knjiznica.posudiKnjigu("2", "4");
+            knjiznica.posudiKnjigu("2", "3");
+            knjiznica.posudiKnjigu("2", "3");
+            knjiznica.posudiKnjigu("2", "3");
+            knjiznica.posudiKnjigu("2", "3");
+
+            knjiznica.ispisKorisnika();
+            Console.WriteLine();
+
+        }
+
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            string ime = imeTextBox.Text;
+            //string prezime = prezimeTextBox.Text;
+            string prezime = "test";
+
+            if(ime != "" && prezime != "")
+            {
+                knjiznica.dodajKorisnika(ime);
+                korisniciList.Items.Clear();
+                foreach(Osoba o in knjiznica.vratiKorisnike())
+                {
+                    korisniciList.Items.Add(o.getIme());
+                }
+            }
+        }
+    }
+
+    public class Knjiga
+    {
+        private static int id_counter = 0;
+        private string id;
+        private string naziv;
+        public bool posudeno = false;
+
+
+        public Knjiga(string naziv)
+        {
+            this.id = id_counter.ToString();
+            this.naziv = naziv;
+
+            id_counter++;
+        }
+
+        public string getNaziv()
+        {
+            return this.naziv;
+        }
+
+        public string getId()
+        {
+            return this.id;
+        }
+    }
+
+    public class Osoba
+    {
+        public List<Knjiga> knjige = new List<Knjiga>();
+        private static int id_counter = 0;
+        private string ime;
+        private string id;
+
+        public Osoba(string ime)
+        {
+            this.id = id_counter.ToString();
+            this.ime = ime;
+
+            id_counter++;
+        }
+
+        public void ispisKnjiga(string padding = "")
+        {
+            foreach (Knjiga k in this.knjige)
+            {
+                Console.WriteLine(padding + k.getId() + " -> " + k.getNaziv());
+            }
+        }
+
+
+        public string getID()
+        {
+            return this.id;
+        }
+
+        public string getIme()
+        {
+            return this.ime;
+        }
+
+    }
+
+    public class Knjiznica
+    {
+        private List<Knjiga> knjige = new List<Knjiga>();
+        private List<Osoba> korisnici = new List<Osoba>();
+
+        public List<Osoba> vratiKorisnike()
+        {
+            return this.korisnici;
+        }
+
+        public List<Knjiga> vratiKnjige()
+        {
+            return this.knjige;
+        }
+
+        public void dodajKnjigu(string naziv)
+        {
+            knjige.Add(new Knjiga(naziv));
+        }
+
+        public void dodajKorisnika(string ime)
+        {
+            korisnici.Add(new Osoba(ime));
+        }
+
+        public void ispisKnjiga()
+        {
+            foreach (Knjiga k in this.knjige)
+            {
+                Console.WriteLine(k.getId() + ": " + k.getNaziv());
+            }
+        }
+
+        public void ispisKorisnika()
+        {
+            foreach (Osoba o in this.korisnici)
+            {
+                Console.WriteLine(o.getID() + ", " + o.getIme());
+                o.ispisKnjiga(padding: "\t");
+            }
+        }
+
+        public int korisnikIndex(string korisnik_id)
+        {
+            int i = 0;
+            foreach (Osoba k in this.korisnici)
+            {
+                if (k.getID() == korisnik_id)
+                {
+                    return i;
+                }
+                i++;
+            }
+            return -1;
+        }
+
+        public int knjigaIndex(string knjiga_id)
+        {
+            int i = 0;
+            foreach (Knjiga k in this.knjige)
+            {
+                if (k.getId() == knjiga_id)
+                {
+                    return i;
+                }
+                i++;
+            }
+            return -1;
+        }
+
+        public void posudiKnjigu(string user_id, string knjiga_id)
+        {
+            int o_index = korisnikIndex(user_id);
+            int k_index = knjigaIndex(knjiga_id);
+
+            if (this.knjige[k_index].posudeno == false)
+            {
+                this.korisnici[o_index].knjige.Add(this.knjige[k_index]);
+                this.knjige[k_index].posudeno = true;
+            }
+            else
+            {
+                Console.WriteLine("\t\t\tERROR: Knjiga " + knjiga_id + " je posudena");
+            }
+
+        }
+    }
+}
